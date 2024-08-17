256cb90559869c4425b4a9486a90308173f6417f
SamaAlsu <amf9080@gmail.com>
2024-08-17 22:31:56 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome'); });
+Route::get('/x', function () {
+    return view('app');});
+
+Route::get('/2', function () {
+    return view('page2');});
+
